2e17d9b209e991170d1016aa3fbe1e5cce86196b
pgsv <pgservice.engineer@gmail.com>
2021-06-26 19:46:45 +0900

    カスタム投稿オブジェクトを取得

--- index.php
+++ index.php
@@ -5,6 +5,29 @@
         <div class="front_sldr"><?php echo do_shortcode('[smartslider3 slider=4]'); ?>
         </div>
         <?php include('template-parts/news.php'); ?>
+
+        <?php
+            function clog($data) {
+            echo '<script>';
+            echo 'console.log('. json_encode($data) .')';
+            echo '</script>';}
+        ?>
+        <?php 
+            $page = get_page_by_path('custom-field');
+            $id = $page->ID;
+            $field_medaka = get_field_object('medaka', $id);    
+            // echo $field_medaka;
+        ?>
+
+        <img src="<?php echo $field_medaka['value']['guid']; ?>" />
+
+        <?php
+            $page = get_page_by_path('custom-field');
+            $id = $page->ID;
+            $field_desc = get_field_object('description', $id);
+            echo $field_desc['value'];
+        ?>
+        <?php clog( $field_medaka); ?>
         <section class="">
             <?php include('template-parts/products.php'); ?>
         </section>
