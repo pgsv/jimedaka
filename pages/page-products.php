78b1537bd862e40658086001c6009a9c85665ef7
pgsv <pgservice.engineer@gmail.com>
2021-07-02 18:42:06 +0900

    関数コメント追加

--- pages/page-products.php
+++ pages/page-products.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
 <h1><?php the_title(); ?>
 </h1>
-<?php echo do_shortcode('[products limit="8" paginate="true"]'); ?>
+<?php //get_template_part('../template_parts/products');?>
+<?php include_once(dirname(__FILE__) ."/../template-parts/products.php");?>
+<?php //echo do_shortcode('[products limit="8" paginate="true"]');?>
 <?php get_footer();
