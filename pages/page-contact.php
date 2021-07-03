2d6d3fbc7c9e2269873ffcf6be5a280ed70773e2
pgsv <pgservice.engineer@gmail.com>
2021-07-03 18:02:58 +0900

    カレンダー表示

--- pages/page-contact.php
+++ pages/page-contact.php
@@ -3,6 +3,7 @@
   <?php if (have_posts()): while (have_posts()):the_post(); ?>
   <h1><?php the_title(); ?>
   </h1>
+  <?php the_widget('bizcalendarwidget'); ?>
   <p><?php the_content(); ?>
   </p>
   <?php endwhile; endif; ?>
