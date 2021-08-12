916b2fe0b909552a339c66adb85f41a4f8a53dfc
pgsv <pgservice.engineer@gmail.com>
2021-08-12 23:39:21 +0900

    slickファイル読み込み設定

--- header.php
+++ header.php
@@ -9,7 +9,17 @@
         href="<?php echo esc_url(get_template_directory_uri().'/style.css'); ?>">
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"
+        href="<?php echo esc_url(get_template_directory_uri().'/assets/slick/slick.css'); ?>"
+        media="screen" />
+    <link rel="stylesheet" type="text/css"
+        href="<?php echo esc_url(get_template_directory_uri().'/assets/slick/slick-theme.css'); ?>"
+        media="screen" />
+    <script
+        src="<?php echo esc_url(get_template_directory_uri()."/assets/slick/slick.min.js");?>">
+    </script>
     <?php wp_head(); ?>
+
 </head>
 
 <body>
