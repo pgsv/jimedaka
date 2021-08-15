46f8f2f3dc34e095b5f1585c6364da74da5e0b9a
pgsv <pgservice.engineer@gmail.com>
2021-08-15 22:07:18 +0900

    注文メールのお客様名をフルネームに変更

--- woocommerce/emails/customer-completed-order.php
+++ woocommerce/emails/customer-completed-order.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Customer processing order email
+ * Customer completed order email
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/emails/customer-processing-order.php.
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/customer-completed-order.php.
  *
  * HOWEVER, on occasion WooCommerce will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
@@ -27,10 +27,8 @@ do_action('woocommerce_email_header', $email_heading, $email); ?>
 <?php /* translators: %s: Customer first name */ ?>
 <p><?php printf(esc_html__('Hi %s,', 'woocommerce'), esc_html($order->get_billing_last_name() .' '.$order->get_billing_first_name())); ?>
 </p>
-<?php /* translators: %s: Order number */ ?>
-<p><?php printf(esc_html__('Just to let you know &mdash; we\'ve received your order #%s, and it is now being processed:', 'woocommerce'), esc_html($order->get_order_number())); ?>
+<p><?php esc_html_e('We have finished processing your order.', 'woocommerce'); ?>
 </p>
-
 <?php
 
 /*
