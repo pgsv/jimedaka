5124e3616357307359675b6d6893fd6c29ca05fd
pgsv <pgservice.engineer@gmail.com>
2022-01-03 17:38:54 +0900

    注文メールの配達日時を追加

--- woocommerce/emails/email-addresses.php
+++ woocommerce/emails/email-addresses.php
@@ -27,21 +27,18 @@ $shipping   = $order->get_formatted_shipping_address();
 <table id="addresses" cellspacing="0" cellpadding="0"
 	style="width: 100%; vertical-align: top; margin-bottom: 40px; padding:0;border:0;">
 	<tr>
-		<td width="15%" style="border-bottom: 1px solid #e9e9e9;border-top: 1px solid #e9e9e9;">
-			<h2 style="color: #636363;">ご請求先</h2>
+		<td width="20%" style="border-bottom: 1px solid #e9e9e9;border-top: 1px solid #e9e9e9; color: #636363">
+			ご請求先
 		</td>
-		<td style="border-bottom: 1px solid #e9e9e9; border-top: 1px solid #e9e9e9; text-align:<?php echo esc_attr($text_align); ?>; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif; padding:0;"
-			valign="top" width="50%">
-			<!-- <h2><?php //esc_html_e('Billing address', 'woocommerce'); ?>
-			</h2> -->
+		<td style="font-size: 14px; border-bottom: 1px solid #e9e9e9; border-top: 1px solid #e9e9e9; text-align:<?php echo esc_attr($text_align); ?>; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif; padding:0;"
+			valign="top">
 
 			<address class="address" style="border:0;font-size: 14px;">
-				<?php //echo wp_kses_post($address ? $address : esc_html__('N/A', 'woocommerce')); ?>
 				<?php
 				$billing_address = wp_kses_post($address ? $address : esc_html__('N/A', 'woocommerce'));  
 				$billing_address = explode('<br />', $billing_address);
 				echo $billing_address[3] . '<br />';
-				echo $billing_address[0] . ' ' . $billing_address[1] . ' ' . $billing_address[2] . '<br />';
+				echo $billing_address[0] . ' ' . $billing_address[1] . ' ' . $billing_address[2];
 				?>
 				<?php if ($order->get_billing_phone()) : ?>
 				<br /><?php echo wc_make_phone_clickable($order->get_billing_phone()); ?>
@@ -54,21 +51,18 @@ $shipping   = $order->get_formatted_shipping_address();
 	</tr>
 	<tr>
 		<?php if (! wc_ship_to_billing_address_only() && $order->needs_shipping_address() && $shipping) : ?>
-		<td width="15%" style="border-bottom: 1px solid #e9e9e9;">
-			<h2 style="color: #636363;">お届け先</h2>
+		<td style="border-bottom: 1px solid #e9e9e9; color: #636363">
+			お届け先
 		</td>
-		<td style="border-bottom: 1px solid #e9e9e9; text-align:<?php echo esc_attr($text_align); ?>; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif; padding:0;"
-			valign="top" width="50%">
-			<!-- <h2><?php //esc_html_e('Shipping address', 'woocommerce'); ?>
-			</h2> -->
+		<td style="font-size: 14px; border-bottom: 1px solid #e9e9e9; text-align:<?php echo esc_attr($text_align); ?>; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif; padding:0;"
+			valign="top">
 
 			<address class="address" style="border:0; font-size: 14px;">
-				<?php //echo wp_kses_post($shipping); ?>
 				<?php
 				$shipping_address = wp_kses_post($shipping);
 				$shipping_address = explode('<br />', $shipping_address);
 				echo $shipping_address[3] . '<br />';
-				echo $shipping_address[0] . ' ' . $shipping_address[1] . ' ' . $shipping_address[2] . '<br />';
+				echo $shipping_address[0] . ' ' . $shipping_address[1] . ' ' . $shipping_address[2];
 				?>
 				<?php if ($order->get_shipping_phone()) : ?>
 				<br /><?php echo wc_make_phone_clickable($order->get_shipping_phone()); ?>
@@ -76,5 +70,14 @@ $shipping   = $order->get_formatted_shipping_address();
 			</address>
 		</td>
 		<?php endif; ?>
+	</tr>
+	<tr>
+		<?php
+		$delivery_date = get_post_meta($order->get_id(), 'wc4jp-delivery-date', true);
+		$delivery_time = get_post_meta($order->get_id(), 'wc4jp-delivery-time-zone', true);
+		$delivery_date_time = '<div>配達指定日：' . ($delivery_date ? $delivery_date : '指定なし') . '</div><div>配達時間帯：' . ($delivery_time ? $delivery_time : '指定なし') . '</div>';
+		?>
+		<td style="border-bottom: 1px solid #e9e9e9; margin:0; color: #636363">配達指定</td>
+		<td style="padding: 12px;font-size: 14px;color: #636363; border-bottom: 1px solid #e9e9e9; text-align:<?php echo esc_attr($text_align); ?>; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;"><?php echo $delivery_date_time; ?></td>
 	</tr>
 </table>
