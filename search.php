694ab5a3b57a0a7a8f872a2d6f09f0aed7f69f89
pgsv <pgservice.engineer@gmail.com>
2022-01-06 19:21:14 +0900

    検索結果の表示崩れを改修

--- search.php
+++ search.php
@@ -4,31 +4,37 @@
     
         <?php include(dirname(__FILE__).'/template-parts/product_aside.php');?>
         <div class="products-wrapper">
-            <main class="products-list">
-            <h1 class="products-title">
-            <?php
-            if (isset($_GET['s']) && empty($_GET['s'])) {
-                echo '検索キーワード未入力'; // 検索キーワードが未入力の場合のテキストを指定
-            } else {
-            echo '“'.$_GET['s'] .'”の検索結果：'.$wp_query->found_posts .'件'; // 検索キーワードと該当件数を表示
-            }
-            ?>
-            </h1>
-                <?php if (have_posts() && get_post_type() === 'product'): ?>
-                <ul class="clearfix">
-                    <?php while (have_posts()): the_post(); ?>
-                    <?php 
-                    $wc_product = wc_get_product(get_the_ID()); 
-                    if ( $wc_product->is_in_stock() ):
+            <main class="products-contents">
+                <h1 class="products-title">
+                    <?php
+                    if (isset($_GET['s']) && empty($_GET['s'])) {
+                        echo '検索キーワード未入力'; // 検索キーワードが未入力の場合のテキストを指定
+                    } else {
+                    echo '“'.$_GET['s'] .'”の検索結果：'.$wp_query->found_posts .'件'; // 検索キーワードと該当件数を表示
+                    }
                     ?>
-                    <li class="products-list-item">
-                        <?php the_product_link_html(get_the_ID()); ?>
-                    </li>
-                    <?php endif; endwhile; ?>
-                </ul>
-                <?php else: ?>
-                <div class="">検索キーワードにマッチする商品はありませんでした。</div>
-                <?php endif; ?>
+                </h1>
+                <?php 
+                if (have_posts() && get_post_type() === 'product'): 
+                    ?>
+                    <ul class="clearfix">
+                        <?php while (have_posts()): the_post(); ?>
+                        <?php 
+                        $wc_product = wc_get_product(get_the_ID()); 
+                        if ( $wc_product->is_in_stock() ):
+                        ?>
+                        <li class="products-list-item">
+                            <?php the_product_link_html(get_the_ID()); ?>
+                        </li>
+                        <?php endif; endwhile; ?>
+                    </ul>
+                    <?php 
+                else: 
+                    ?>
+                    <div class="">検索キーワードにマッチする商品はありませんでした。</div>
+                    <?php 
+                endif; 
+            ?>
             </main>
         </div>
     </div>
